40d8699e296f7d846b84374143d636f5887fff5d
Robert Palmer <robd003@users.noreply.github.com>
2024-02-17 06:38:53 -0500

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+if [ -z "$B2_APPLICATION_KEY_ID" ]; then
+  echo "Error: Missing B2_APPLICATION_KEY_ID"
+  exit 1
+fi
+
+if [ -z "$B2_APPLICATION_KEY" ]; then
+  echo "Error: Missing B2_APPLICATION_KEY"
+  exit 1
+fi
+
+if [ -z "$SOURCE_DIR" ]; then
+  echo "Error: Missing SOURCE_DIR"
+  exit 1
+fi
+
+if [ -z "$B2_BUCKET" ]; then
+  echo "Error: Missing B2_BUCKET"
+  exit 1
+fi
+
+b2 sync --delete --replaceNewer ${SOURCE_DIR} ${B2_BUCKET}
